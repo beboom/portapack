581cabef1d71f2c56517de523c4dae3bf4cc9824
beboom <igorgf@gmail.com>
2022-06-30 02:22:25 -0300

    Alteração do logo superior e centralização da splash screen

--- firmware/application/ui_navigation.cpp
+++ firmware/application/ui_navigation.cpp
@@ -680,7 +680,7 @@ BMPView::BMPView(NavigationView& nav) {
 
 void BMPView::paint(Painter&) {
 	if(!portapack::display.drawBMP2({ 0, 0 }, "splash.bmp"))
-		portapack::display.drawBMP({(240 - 230) / 2, (320 - 50) / 2 - 10}, splash_bmp, false);
+		portapack::display.drawBMP({(240 - 64) / 2, (320 - 80) / 2 - 10}, splash_bmp, false);
 }
 
 /* NotImplementedView ****************************************************/
